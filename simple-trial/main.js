6f951294c7448e52237415233b12c47cc841db7d
Guillermo Gutierrez <guillegr123@gmail.com>
2018-12-21 16:35:16 -0600

    Register using custom data or Onsip random data

--- simple-trial/main.js
+++ simple-trial/main.js
@@ -1,18 +1,66 @@
 // sipjs_demos.js
-//
-// Even though both "Alice" and "Bob" are running on the same computer,
-// this demo behaves as if the dialog was an SIP call over a network.
 
-var data = {
-    userUri: 'usercode@myrealm.com',
-    privateUser: 'username',
-    password: 'password',
-    displayName: 'displayName',
-    wsServer: 'wss://websockets.url.com:5065',
-    directory: {
-        personA: '5558889999',  // Guillermo
+var URL = window.URL || window.webkitURL;
+
+function getCookie(key) {
+    var re = new RegExp("(?:(?:^|.*;\s*) ?" + key + "\s*\=\s*([^;]*).*$)|^.*$");
+    return document.cookie.replace(re, "$1");
+
+}
+
+// This demo uses unauthenticated users on the "sipjs.onsip.com" demo domain.
+// To allow multiple users to run the demo without playing a game of
+// chatroulette, we give both callers in the demo a random token and then only
+// make calls between users with these token suffixes.
+// So, you still might run into a user besides yourself.
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i)
+        result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+}
+// Each session gets a token that expires 1 day later. This is so we minimize
+// the number of users we register for the SIP domain, because SIP hosts
+// generally have limits on the number of registered users you may have in total
+// or over a period of time.
+var token = getCookie('onsipToken');
+if (token === '') {
+    token = randomString(32, ['0123456789',
+                              'abcdefghijklmnopqrstuvwxyz',
+                              'ABCDEFGHIJKLMNOPQRSTUVWXYZ'].join(''));
+    var d = new Date();
+    d.setTime(d.getTime() + 1000*60*60*24); // expires in 1 day
+    document.cookie = ('onsipToken=' + token + ';'
+                       + 'expires=' + d.toUTCString() + ';');
+}
+var domain = 'sipjs.onsip.com';
+var aliceURI      = 'alice.' + window.token + '@' + domain;
+var aliceName     = 'Alice';
+
+var bobURI        = 'bob.' + window.token + '@' + domain;
+var bobName       = 'Bob';
+
+function getCustomUA() {
+    return {
+        traceSip: true,
+        uri: 'myrealm.com',
+        displayName: 'displayName',
+        wsServers: ['wss://websockets.url.com:1111'],
+        authorizationUser: 'username',
+        password: 'password'
     }
 }
+
+function getRandomOnSipUA() {
+    return {
+        traceSip: true,
+        uri: 'alice.' + window.token + '@' + domain,
+        displayName: 'Alice ' + window.token,
+        userAgentString: SIP.C.USER_AGENT + " sipjs.com"
+    }
+}
+
+var currentUA = getRandomOnSipUA();
 
 // Function: createSimple
 //   creates a SIP.js Simple instance with the given arguments plugged into the
@@ -24,6 +72,7 @@ var data = {
 //   remoteVideo: the DOM element id of the video for the remote
 //   buttonId: the DOM element id of the button for that user
 function createSimple(remoteVideo, buttonId) {
+    var recipient = document.getElementById('recipient');
     var remoteVideoElement = document.getElementById(remoteVideo);
     var localVideoElement = document.getElementById('localVideo');
     var button = document.getElementById(buttonId);
@@ -37,27 +86,19 @@ function createSimple(remoteVideo, buttonId) {
                 audio: localVideoElement
             }
         },
-        ua: {
-            traceSip: true,
-            uri: data.userUri,
-            displayName: data.displayName,
-            //userAgentString: SIP.C.USER_AGENT + " sipjs.com"
-            wsServers: [data.wsServer],
-            authorizationUser: data.privateUser,
-            password: data.password
-        }
+        ua: getCustomUA()
     };
     var simple = new SIP.Web.Simple(configuration);
 
     // Adjust the style of the demo based on what is happening
     simple.on('ended', function() {
         remoteVideoElement.style.visibility = 'hidden';
-        button.firstChild.nodeValue = 'video';
+        button.firstChild.nodeValue = 'Call';
     });
 
     simple.on('connected', function() {
         remoteVideoElement.style.visibility = 'visible';
-        button.firstChild.nodeValue = 'hang up';
+        button.firstChild.nodeValue = 'Hang up';
     });
 
     simple.on('ringing', function() {
@@ -68,7 +109,7 @@ function createSimple(remoteVideo, buttonId) {
         // No current call up
         if (simple.state === SIP.Web.Simple.C.STATUS_NULL ||
             simple.state === SIP.Web.Simple.C.STATUS_COMPLETED) {
-            simple.call(data.directory.personA);
+            simple.call(recipient.value);
         } else {
             simple.hangup();
         }
@@ -81,12 +122,15 @@ function createSimple(remoteVideo, buttonId) {
 (function () {
     if (!window.RTCPeerConnection) return;
 
+    var usernameElement = document.getElementById('username');
+
     // Now we do SIP.js stuff
     var aliceSimple = createSimple('video-of-bob', 'alice-video-button');
 
     // We want to only run the demo if all users for the demo can register
     var markAsRegistered = function () {
         console.log('User registered!');
+        usernameElement.innerHTML = currentUA.uri;
     };
     var failRegistration = function () {
         console.log('User registration failed');
