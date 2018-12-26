d7e4a86ee778066ea0d31046a79d086e0ac56626
Guillermo Gutierrez <guillegr123@gmail.com>
2018-12-26 10:46:24 -0600

    Added fields to allow call between two different windows

--- simple-trial/main.js
+++ simple-trial/main.js
@@ -1,6 +1,7 @@
 // sipjs_demos.js
 
 var URL = window.URL || window.webkitURL;
+var domain = 'sipjs.onsip.com';
 
 function getCookie(key) {
     var re = new RegExp("(?:(?:^|.*;\s*) ?" + key + "\s*\=\s*([^;]*).*$)|^.*$");
@@ -23,22 +24,8 @@ function randomString(length, chars) {
 // the number of users we register for the SIP domain, because SIP hosts
 // generally have limits on the number of registered users you may have in total
 // or over a period of time.
-var token = getCookie('onsipToken');
-if (token === '') {
-    token = randomString(32, ['0123456789',
-                              'abcdefghijklmnopqrstuvwxyz',
-                              'ABCDEFGHIJKLMNOPQRSTUVWXYZ'].join(''));
-    var d = new Date();
-    d.setTime(d.getTime() + 1000*60*60*24); // expires in 1 day
-    document.cookie = ('onsipToken=' + token + ';'
-                       + 'expires=' + d.toUTCString() + ';');
-}
-var domain = 'sipjs.onsip.com';
-var aliceURI      = 'alice.' + window.token + '@' + domain;
-var aliceName     = 'Alice';
 
-var bobURI        = 'bob.' + window.token + '@' + domain;
-var bobName       = 'Bob';
+
 
 function getCustomUA() {
     return {
@@ -51,17 +38,6 @@ function getCustomUA() {
     }
 }
 
-function getRandomOnSipUA() {
-    return {
-        traceSip: true,
-        uri: 'alice.' + window.token + '@' + domain,
-        displayName: 'Alice ' + window.token,
-        userAgentString: SIP.C.USER_AGENT + " sipjs.com"
-    }
-}
-
-var currentUA = getRandomOnSipUA();
-
 // Function: createSimple
 //   creates a SIP.js Simple instance with the given arguments plugged into the
 //   configuration. This is a standard Simple instance for WebRTC calls.
@@ -71,7 +47,7 @@ var currentUA = getRandomOnSipUA();
 //   displayName: what name we should display the user as
 //   remoteVideo: the DOM element id of the video for the remote
 //   buttonId: the DOM element id of the button for that user
-function createSimple(remoteVideo, buttonId) {
+function createSimple(remoteVideo, buttonId, userName, userURI) {
     var recipient = document.getElementById('recipient');
     var remoteVideoElement = document.getElementById(remoteVideo);
     var localVideoElement = document.getElementById('localVideo');
@@ -86,7 +62,12 @@ function createSimple(remoteVideo, buttonId) {
                 audio: localVideoElement
             }
         },
-        ua: getCustomUA()
+        ua: (!userURI) ? getCustomUA() : {
+            traceSip: true,
+            uri: userURI,
+            displayName: userName,
+            userAgentString: SIP.C.USER_AGENT + " sipjs.com"
+        }
     };
     var simple = new SIP.Web.Simple(configuration);
 
@@ -102,13 +83,16 @@ function createSimple(remoteVideo, buttonId) {
     });
 
     simple.on('ringing', function() {
+        if (window.confirm('Incoming call...')) {
       simple.answer();
+        }
     });
 
     button.addEventListener('click', function() {
         // No current call up
         if (simple.state === SIP.Web.Simple.C.STATUS_NULL ||
             simple.state === SIP.Web.Simple.C.STATUS_COMPLETED) {
+            window.alert('Calling to ' + recipient.value);
             simple.call(recipient.value);
         } else {
             simple.hangup();
@@ -119,32 +103,59 @@ function createSimple(remoteVideo, buttonId) {
 }
 
 
-(function () {
+function register(userName, userURI) {
     if (!window.RTCPeerConnection) return;
 
     var usernameElement = document.getElementById('username');
 
     // Now we do SIP.js stuff
-    var aliceSimple = createSimple('video-of-bob', 'alice-video-button');
+    var simple = createSimple('video-of-bob', 'alice-video-button', userName, userURI);
 
     // We want to only run the demo if all users for the demo can register
     var markAsRegistered = function () {
         console.log('User registered!');
-        usernameElement.innerHTML = currentUA.uri;
+        usernameElement.innerHTML = userURI;
     };
     var failRegistration = function () {
         console.log('User registration failed');
     };
     // We don't want to proceed until we've registered all users.
     // For each registered user, increase the counter.
-    aliceSimple.on('registered', markAsRegistered);
+    simple.on('registered', markAsRegistered);
     // If any registration fails, then we need to disable the app and tell the
     // user that we could not register them.
-    aliceSimple.on('registrationFailed', failRegistration);
+    simple.on('registrationFailed', failRegistration);
 
     // Unregister the user agents and terminate all active sessions when the
     // window closes or when we navigate away from the page
     window.onunload = function () {
-        aliceSimple.stop();
+        //simple.stop();
     };
+    };
+
+(function () {
+    var btnGenerateSuffix = document.getElementById('button_suffix'),
+        inputCallerSuffix = document.getElementById('caller_suffix'),
+        inputCallerName = document.getElementById('caller_name'),
+        btnRegister = document.getElementById('button_register');
+
+    btnGenerateSuffix.addEventListener('click', function() {
+        inputCallerSuffix.value = randomString(32, ['0123456789',
+            'abcdefghijklmnopqrstuvwxyz',
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ'].join(''));
+    });
+
+    btnRegister.addEventListener('click', function() {
+        var callerName = inputCallerName.value,
+            callerSuffix = inputCallerSuffix.value,
+            callerURI = callerName + '.' + callerSuffix + '@' + domain;
+
+        var token = callerSuffix;
+        var d = new Date();
+        d.setTime(d.getTime() + 1000*60*60*24); // expires in 1 day
+        document.cookie = ('onsipToken=' + token + ';'
+                            + 'expires=' + d.toUTCString() + ';');
+        
+        register(callerName, callerURI);
+    });
 })();
